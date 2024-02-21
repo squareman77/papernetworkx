8a818fc4b943e29aeb4722da41012c30ee6c4671
squareman77 <75134215+squareman77@users.noreply.github.com>
2024-02-21 11:11:22 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,13 +8,20 @@ from networkx import algorithms
 
 
 for i in range(100):
+    #Size of graph
     a = 1000 + 50*i
+    #Noise edge probability
     b=(np.log(a))/a
+    #Count of number of edges with at least the centrality of edge 0-1 
     c=0
 
+    #Create UA graph with size a
     graph = nx.gn_graph(a, kernel=lambda x: 1)
+    #Make undirected
     graph= graph.to_undirected()
+    #Erdos renyi noise edges
     graph2 = nx.erdos_renyi_graph(a,b)
+    #Compose graphs
     graph = nx.compose(graph, graph2)
 
     dictionary = algorithms.edge_betweenness_centrality(graph, k=None, normalized=False, weight=None, seed=None)
